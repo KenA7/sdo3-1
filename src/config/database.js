a6a454b5b0005487e122d2b1a8be3b2be8a00218
s3703885 <s3703885@student.rmit.edu.au>
2021-05-31 23:23:49 +0800

    update

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: process.env.DB_URL || "mongodb://AWS:12345678@todo-db-test-docdb-cluster.cluster-chyox4cuah7f.us-east-1.docdb.amazonaws.com:27017"
+  mongoURI: process.env.DB_URL || "mongodb://localhost:27017/tododb"
 }
